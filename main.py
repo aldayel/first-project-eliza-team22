a1944e7015f8419d05ea87fbbe55cedc8d2f103f
Rahaf <102740867+xhr7@users.noreply.github.com>
2025-03-07 20:33:31 +0300

    First version of the UI.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import streamlit as st
-"""
-Hi there friend! You came to your perfect virtual chill zone spot. If you are 
-"""
-
-# inp = input("What would you like ")
-
+import time
 with open('./styles/main.css') as f:
     css = f.read()
 
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-st.page_link("main.py", label="Information About The App", icon="🏠")
-st.page_link("pages/chat.py", label="Chat with your favorite coffee expert!", icon="1️⃣")
-st.page_link("pages/team.py", label="About The Team", icon="2️⃣")
+
+st.title("Welcome to our project ☕")
+
+
+
+st.markdown(
+    '''
+    <div class="text-box">
+        <p class="animated-text">Enjoy your coffee experience with our chatbot!</p>
+    </div>
+    ''',
+    unsafe_allow_html=True
+)
